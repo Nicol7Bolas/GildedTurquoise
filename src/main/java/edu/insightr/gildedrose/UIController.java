387bf200ba99fa5294c595074616093d0f4e4769
GuihaumeB <guihaume.buevoz@devinci.fr>
2018-11-05 19:07:44 +0100

    Edited UIController.java

--- src/main/java/edu/insightr/gildedrose/UIController.java
+++ src/main/java/edu/insightr/gildedrose/UIController.java
@@ -1,18 +1,24 @@
 package edu.insightr.gildedrose;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class UIController {
+public class UIController implements Initializable {
+    @FXML
     public Label oui;
 
     public void Test(ActionEvent actionEvent) {
         oui.setText("Oui.");
     }
 
-    public static void main(String[] args){
-        UIController ui = new UIController();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
 
