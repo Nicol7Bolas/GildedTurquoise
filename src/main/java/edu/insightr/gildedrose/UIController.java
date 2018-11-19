743ca2cdf476f857edca532a63f42d0b89ac8362
GuihaumeB <guihaume.buevoz@devinci.fr>
2018-11-19 17:04:16 +0100

    Interface à modifier

--- src/main/java/edu/insightr/gildedrose/UIController.java
+++ src/main/java/edu/insightr/gildedrose/UIController.java
@@ -4,16 +4,24 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class UIController implements Initializable {
     @FXML
-    public Label oui;
+    private TableView<Item> tableView1;
+    private TableColumn<Item, String> itemName;
+    private TableColumn<Item, String> itemSellIn;
+    private TableColumn<Item, String> itemQuality;
+    private ComboBox comboBox1;
+    private TextField SellIn;
+    private TextField Quality;
 
-    public void Test(ActionEvent actionEvent) {
-        oui.setText("Oui.");
+    public void DisplayInventory(ActionEvent actionEvent) {
+        Inventory inv = new Inventory();
+
     }
 
     @Override
