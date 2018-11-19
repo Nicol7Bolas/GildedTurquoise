0c83bb7c80e0620324ba6a956ae3dffee5222572
GuihaumeB <guihaume.buevoz@devinci.fr>
2018-11-19 16:33:00 +0100

    Interface à modifier

--- src/main/java/edu/insightr/gildedrose/App.java
+++ src/main/java/edu/insightr/gildedrose/App.java
@@ -10,9 +10,9 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         try{
-            Parent root = FXMLLoader.load(getClass().getResource("interface.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/view/interface.fxml"));
             Scene scene = new Scene(root, 300, 275);
-            scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+            scene.getStylesheets().add(getClass().getResource("/view/styles.css").toExternalForm());
             primaryStage.setTitle("Interface");
             primaryStage.setScene(scene);
             primaryStage.show();
